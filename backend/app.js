d0beef9236e4b3b207717d48af26751d0de23bc5
hiqb269 <hiqb269@aucklanduni.ac.nz>
2024-08-29 16:52:16 +1200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const logger = require("./utils/logger.js");
 var userController = require("./controllers/userController");
 var commentController = require("./controllers/commentController");
 var profileController = require("./controllers/profileController");
+var userProfileController =require("./controllers/userProfileController.js");
 
 const { connect } = require("./daos/mongodbClient");
 const swaggerController = require("./controllers/swaggerController");
@@ -26,6 +27,7 @@ connect().then(() => {
   app.use("/api/users", userController);
   app.use("/api/comments", commentController);
   app.use("/api/profiles", profileController);
+  app.use("/api/userprofile", userProfileController);
 
   // setup swagger ui
   swaggerController(app);
