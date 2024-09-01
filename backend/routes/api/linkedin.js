20fdb3a9d1d405c2ea22bff8496194c6cee52c76
karenzhang539 <161431267+karenzhang539@users.noreply.github.com>
2024-09-01 22:53:30 +1200

    Create Structure

---
+++ backend/routes/api/linkedin.js
@@ -0,0 +1,10 @@
+//SignIn
+//get token and profile - email address
+//use email address to query database to get userid, return to front-end
+
+
+
+//SignUp
+//get token and profile - email address and name
+//create a userId and save email address and name to database
+//return userId to front-end
