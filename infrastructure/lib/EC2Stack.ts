e73399d3afd559ebab878fe9fa048b77b66343b2
hiqb269 <hiqb269@aucklanduni.ac.nz>
2024-09-05 10:56:46 +1200

    Revert "Allow inbound traffic on port 8888"
    This reverts commit 9f28e18ae630ea54a898ce7d51f225f5745f2af2.

--- infrastructure/lib/EC2Stack.ts
+++ infrastructure/lib/EC2Stack.ts
@@ -36,15 +36,13 @@ export class EC2Stack extends Stack {
     const securityGroup = new SecurityGroup(this, "AppSG", {
       vpc,
       description:
-        "Allow inbound from ports 3000(node) and 5000(react) and 8080(db admin) and 8888 (pgadmin)",
+        "Allow inbound from ports 3000(node) and 5000(react) and 8080(db admin)",
       allowAllOutbound: true,
     });
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22));
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3000));
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(5000));
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(8080));
-    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(8888));
-
     const ec2Instance = new Instance(this, "EC2Instance", {
       vpc,
       // free tier t2.micro
