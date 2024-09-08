ef54500a94c3f66ca9809e4172edebb91fa88597
karenzhang539 <161431267+karenzhang539@users.noreply.github.com>
2024-09-08 22:36:19 +1200

    Delete linkedinService.js

--- backend/services/linkedinService.js
+++
@@ -1,8 +0,0 @@
-
-//Get Access Token, get authorization code from front end, and make https post request to get a json response to have access token
-//return token
-
-
-//Retrieve Profile Data, use access token to do http get reuqest to get this user profile 
-//return email address, name,  profile photo
-
