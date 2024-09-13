08836dd17639b09a0b920356139b930aaf57a0e2
karenzhang539 <161431267+karenzhang539@users.noreply.github.com>
2024-09-13 21:50:05 +1200

    test

--- backend/tests/controllers/openIdService.test.js
+++ backend/tests/controllers/openIdService.test.js
@@ -9,13 +9,13 @@ const getOpenIdToken = require('../../services/openIDService');
 async function testIDToken() {
   try {
     const grant_type = 'authorization_code';
-    const code = 'AQQ-96uXQCIZbkLdWuTRxNXoIapPxxNFWM4ZlpVN6sbCsUvkhG4JMYtLrjD-LYXbUWDC4K5JZ9bs3DUxBX0OnCYVCyz3T9Sxx14gzISsF5zfoyfrAlbwSZ-ll5HpxyovoX86UwX9BwE2cqWFXQA16mQMUMsUhP0ojsY_QmaROVzP63p5OUU_F4hn_MeX434TAz7hE15-72dukNaIOHE';
+    const code = 'AQTgrUCACAp2xlBqPxauLTJ0-gRV0LaCOyt1tLqItg3qoKmlsZNKG7cMMQVYE71P5UQC_K3b30zTQPLBKt4pr3rToeAL-z7NuN1naZocQUc2Ghrc_-PPJgjSfDtz_65svDP67dado1f-5WhtbEOuUCfUawmRw-qgl6LAbFvhD1z_BXWGHnOCqIS6661xQkHGDFFCtuwCDesHCI10o2g';
     const client_id = process.env.LINKEDIN_CLIENT_ID;
     const client_secret = process.env.LINKEDIN_CLIENT_SECRET;
     const redirect_uri = process.env.LINKEDIN_REDIRECT_URI;
     const url = process.env.LINKEDIN_TOKEN_URL;
     const tokenData = await getOpenIdToken(grant_type, code, client_id, client_secret, redirect_uri,url);
-    console.log('Access Token:', tokenData.email);
+    console.log('Email:', tokenData.email);
     console.log('Token Data:', tokenData);
   } catch (error) {
     console.error('Error during test:', error);
