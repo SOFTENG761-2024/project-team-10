ba039ddf5b2e41297ead4a7a5d31074793dd137d
MarkZhuVUW <zdy120939259@outlook.com>
2024-09-16 09:57:54 +1200

    Update EC2Stack.ts

--- infrastructure/lib/EC2Stack.ts
+++ infrastructure/lib/EC2Stack.ts
@@ -44,7 +44,7 @@ export class EC2Stack extends Stack {
 
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(443));
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22));
-    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
+    // securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3000));
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(5000));
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(8080));
