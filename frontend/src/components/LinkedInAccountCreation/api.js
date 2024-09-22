d98c253735b6acf11889240e789d5f94dd44302f
ukul426 <ukul426@aucklanduni.ac.nz>
2024-09-22 17:58:41 +1200

    Added API function to send the new user detail

---
+++ frontend/src/components/LinkedInAccountCreation/api.js
@@ -0,0 +1,20 @@
+import { useAPI } from "@frontend-ui/components/GlobalProviders";
+
+export const useAccountCreationAPI = () => {
+  const { post, error } = useAPI();
+
+  const createAccount = async (formData) => {
+    try {
+      const response = await post(
+        `${import.meta.env.VITE_BACKEND_API_BASE_URL}/api/accounts/create`, // need to change the api
+        formData,
+      );
+      return response?.data;
+    } catch (error) {
+      console.error("Error creating account:", error);
+      throw error;
+    }
+  };
+
+  return { createAccount, error };
+};
