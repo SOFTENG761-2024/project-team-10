6845b21b1e47a0679c8063b5d62a0140d75633c1
yzhu951 <yzhu951@aucklanduni.ac.nz>
2024-10-06 11:16:38 +1300

    landing page test

---
+++ e2eTests/tests/landing-workflow.test.js
@@ -0,0 +1,35 @@
+const { test, expect, afterEach, beforeEach } = require("@playwright/test");
+require('dotenv').config({ path: './e2eTests/.env' });
+test.beforeEach(async ({ page }) => {
+  await page.goto(`${process.env.REACT_APP_URL}`);
+  await page.waitForTimeout(500);
+});
+test("test can load login page", async ({ page }) => {
+
+  // Get the initial theme
+  const initialTheme = await page.evaluate(() => document.body.getAttribute('data-theme'));
+
+  await page.getByRole('button', { name: 'themeicon' }).click();
+  // Wait for the theme to change (may require a short delay)
+  await page.waitForTimeout(500);
+
+  // Get the switched theme
+  const newTheme = await page.evaluate(() => document.body.getAttribute('data-theme'));
+  // Verify that the theme has changed
+  expect(newTheme).not.toBe(initialTheme);
+  console.log(`Theme changed from ${initialTheme} to ${newTheme}`);
+  // Click the theme switch button again
+  await page.getByRole('button', { name: 'themeicon' }).click();
+
+  // Wait for the theme to change again
+  await page.waitForTimeout(500);
+
+  // Get the theme after switching again
+  const finalTheme = await page.evaluate(() => document.body.getAttribute('data-theme'));
+
+  // Verify that the theme has been switched back to the initial state
+  expect(finalTheme).toBe(initialTheme);
+  console.log(`Theme changed back to ${finalTheme}`);
+
+
+});
