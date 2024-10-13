8d4a85be9147c9a22171342e8cba663bba101d5a
karenzhang539 <161431267+karenzhang539@users.noreply.github.com>
2024-10-13 15:33:16 +1300

    Code clean and readme updated for Tuakiri

--- backend/controllers/tuakiriAuthController.js
+++ backend/controllers/tuakiriAuthController.js
@@ -12,11 +12,6 @@ const passwordService = require("../services/passwordService.js");
 
 env.config({ path: path.resolve(__dirname, "../.env") });
 
-// auth login
-// router.get('/login', async (req,res) => {
-//     res.render('login');
-// })
-
 //auth logout
 router.get("/signout", async (req, res) => {
   //handle with passport
@@ -30,10 +25,10 @@ router.get(
 
 //callback route for Tuakiri to redirect to
 router.get('/tuakiri/redirect', passport.authenticate('tuakiriOpenId', {
-  failureRedirect: "https://www.academicfellows.com/signin", failureMessage: true
+  failureRedirect: process.env.FRONT_END_BASE_URL + '/signin', failureMessage: true
 }), (req, res) => {
   
-    res.redirect("https://www.academicfellows.com/search-profile"); // Redirect to search page if verified
+    res.redirect(process.env.FRONT_END_BASE_URL + '/search-profile'); // Redirect to search page if verified
   
 });
 
@@ -45,32 +40,5 @@ router.get("/current-user", async (req, res) => {
     return res.status(500).json(error);
   }
 });
-
-router.post("/test", async (req, res) => {
-  try {
-    console.log("test");
-    await createAccountEmailService.sendBusinessAccountVerifiedEmail('tableliu@hotmail.com');
-    return res.json("test");
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-});
-
-//end point to update user password
-router.put("/updatePassword/:id", async function updatePassword(req, res)  {
-  try
-  {
-    const id = req.params.id;
-    const hashedPassword = await passwordService.hashPassword(req.body.password);
-    const userprofileObject = { id: id, password: hashedPassword, password_update_datetime: new Date() };
-    const updatedUser = await userProfileService.updateUserProfile(userprofileObject);
-    return res.json(updatedUser);
-  } 
-  catch (error) {
-    console.log(error);
-    return res.status(500).json(error);
-  }
-});
-
 
 module.exports = router;
